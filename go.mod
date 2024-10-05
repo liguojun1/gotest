ba5e0fc0f67d7750b5351a23a63dfe59cd8fe4f1
liguojun <lgj@qq.com>
2024-10-05 11:48:55 +0800

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/liguojun1/gotest
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package gotest
+
+import "fmt"
+
+func Demo() {
+	fmt.Println("hello world")
+}
+
+func echo() {
+	fmt.Println("hello echo")
+}
